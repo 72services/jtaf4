6f6a5d90f74f4ad37175188ca79b45a248671dac
Simon Martinelli <simon@72.services>
2021-07-08 14:11:07 +0200

    Introduced Testcontainers

---
+++ src/test/java/ch/jtaf/service/AbstractTestcontainersTest.java
@@ -0,0 +1,23 @@
+package ch.jtaf.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+public abstract class AbstractTestcontainersTest {
+
+    @Container
+    public static MySQLContainer mySQLContainer = new MySQLContainer("mysql:5.7.34");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+    }
+}
--- src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
+++ src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
@@ -4,11 +4,15 @@ import ch.jtaf.reporting.data.EventsRankingData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class CompetitionRankingServiceTest {
+class CompetitionRankingServiceTest extends AbstractTestcontainersTest{
 
     @Autowired
     private CompetitionRankingService competitionRankingService;
