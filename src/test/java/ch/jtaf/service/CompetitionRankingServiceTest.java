74e8e059bb268192e578b092567a32bdb1ca9739
Simon Martinelli <simon@72.services>
2021-12-29 18:05:57 +0100

    Added NumberAndSheetsServiceTest

--- src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
+++ src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
@@ -1,6 +1,7 @@
 package ch.jtaf.service;
 
 import ch.jtaf.reporting.data.CompetitionRankingData;
+import ch.jtaf.reporting.data.EventsRankingData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,27 @@ class CompetitionRankingServiceTest {
     @Test
     void create_competition_ranking_pdf() {
         byte[] pdf = competitionRankingService.getCompetitionRankingAsPdf(6L);
+
+        assertThat(pdf.length).isGreaterThan(0);
+    }
+
+    @Test
+    void get_events_ranking() {
+        EventsRankingData eventsRanking = competitionRankingService.getEventsRanking(6L);
+
+        assertThat(eventsRanking.name()).isEqualTo("39. Jugendmeisterschaft");
+    }
+
+    @Test
+    void get_events_ranking_pdf() {
+        byte[] pdf = competitionRankingService.getEventRankingAsPdf(6L);
+
+        assertThat(pdf.length).isGreaterThan(0);
+    }
+
+    @Test
+    void get_diplomas_pdf() {
+        byte[] pdf = competitionRankingService.getDiplomasAsPdf(6L);
 
         assertThat(pdf.length).isGreaterThan(0);
     }
---
+++ src/test/java/ch/jtaf/service/NumberAndSheetsServiceTest.java
@@ -0,0 +1,40 @@
+package ch.jtaf.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class NumberAndSheetsServiceTest {
+
+    @Autowired
+    private NumberAndSheetsService numberAndSheetsService;
+
+    @MockBean
+    private UserService userService;
+
+
+    @Test
+    void create_numbers() {
+        byte[] pdf = numberAndSheetsService.createNumbers(1L);
+
+        assertThat(pdf.length).isGreaterThan(0);
+    }
+
+    @Test
+    void create_sheets() {
+        byte[] pdf = numberAndSheetsService.createEmptySheets(1L, 1L);
+
+        assertThat(pdf.length).isGreaterThan(0);
+    }
+
+    @Test
+    void create_empty_sheets() {
+        byte[] pdf = numberAndSheetsService.createEmptySheets(1L, 1L);
+
+        assertThat(pdf.length).isGreaterThan(0);
+    }
+}
