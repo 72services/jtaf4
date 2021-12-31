bbf0640595d5e1b1a7989ef9401c1abadeecbb18
Simon Martinelli <simon@72.services>
2021-12-31 14:26:26 +0100

    TranslationProviderTest added

---
+++ src/test/java/ch/jtaf/ui/translation/TranslationProviderTest.java
@@ -0,0 +1,57 @@
+package ch.jtaf.ui.translation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TranslationProviderTest {
+
+    @Autowired
+    private TranslationProvider translationProvider;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void get_provided_locales() {
+        var providedLocales = translationProvider.getProvidedLocales();
+
+        assertThat(providedLocales).isNotEmpty();
+        assertThat(providedLocales.size()).isEqualTo(2);
+    }
+
+    @Test
+    void get_existing_translation() {
+        var confirm = translationProvider.getTranslation("Confirm", Locale.ENGLISH);
+
+        assertThat(confirm).isEqualTo("Confirm");
+    }
+
+    @Test
+    void get_existing_translation_german() {
+        var confirm = translationProvider.getTranslation("Confirm", Locale.GERMAN);
+
+        assertThat(confirm).isEqualTo("Bestätigen");
+    }
+
+    @Test
+    void get_missing_translation() {
+        String hello = translationProvider.getTranslation("Hello", Locale.ENGLISH);
+
+        assertThat(hello).isEqualTo("!en: Hello");
+    }
+
+    @Test
+    void get_null_translation() {
+        String nothing = translationProvider.getTranslation(null, Locale.ENGLISH);
+
+        assertThat(nothing).isEqualTo("");
+    }
+}
