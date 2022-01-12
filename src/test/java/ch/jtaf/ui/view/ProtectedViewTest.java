e69455e54efa8f9aa80d946d4f3758cf2a38c462
Simon Martinelli <simon@72.services>
2022-01-12 16:37:20 +0100

    Removed public access modifier

--- src/test/java/ch/jtaf/ui/view/ProtectedViewTest.java
+++ src/test/java/ch/jtaf/ui/view/ProtectedViewTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static com.github.mvysny.kaributesting.v10.LocatorJ._get;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ProtectedViewTest extends KaribuTest {
+class ProtectedViewTest extends KaribuTest {
 
     @Test
     void user_without_organization() {
