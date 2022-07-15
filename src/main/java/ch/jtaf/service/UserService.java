fdc1de42c632764d50a4efabd91659c70807637e
Simon Martinelli <simon@72.services>
2022-07-15 11:28:01 +0200

    Back to self hosted

--- src/main/java/ch/jtaf/service/UserService.java
+++ src/main/java/ch/jtaf/service/UserService.java
@@ -1,48 +1,38 @@
 package ch.jtaf.service;
 
 import ch.jtaf.db.tables.records.SecurityUserRecord;
-import ch.jtaf.db.tables.records.UserGroupRecord;
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
 import com.vaadin.flow.i18n.I18NProvider;
 import org.jooq.DSLContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.util.Locale;
 import java.util.UUID;
 
 import static ch.jtaf.db.tables.SecurityGroup.SECURITY_GROUP;
 import static ch.jtaf.db.tables.SecurityUser.SECURITY_USER;
+import static ch.jtaf.db.tables.UserGroup.USER_GROUP;
 
 @SuppressWarnings("ClassCanBeRecord")
 @Service
 public class UserService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
-
     private final DSLContext dsl;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
     private final I18NProvider i18n;
-    private final SendGrid sendGrid;
     private final String publicAddress;
 
-    public UserService(DSLContext dsl, PasswordEncoder passwordEncoder, I18NProvider i18n, SendGrid sendGrid,
+    public UserService(DSLContext dsl, PasswordEncoder passwordEncoder, JavaMailSender mailSender, I18NProvider i18n,
                        @Value("${jtaf.public.address}") String publicAddress) {
         this.dsl = dsl;
         this.passwordEncoder = passwordEncoder;
+        this.mailSender = mailSender;
         this.i18n = i18n;
-        this.sendGrid = sendGrid;
         this.publicAddress = publicAddress;
     }
 
@@ -53,52 +43,40 @@ public class UserService {
         if (count != null && count > 0) {
             throw new UserAlreadyExistException();
         }
-        var user = new SecurityUserRecord();
+        var user = dsl.newRecord(SECURITY_USER);
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
         user.setSecret(passwordEncoder.encode(password));
         user.setConfirmationId(UUID.randomUUID().toString());
         user.setConfirmed(false);
-        dsl.attach(user);
         user.store();
 
         var group = dsl.selectFrom(SECURITY_GROUP).where(SECURITY_GROUP.NAME.eq("USER")).fetchOne();
 
         if (group != null) {
-            var userGroup = new UserGroupRecord(user.getId(), group.getId());
-            dsl.attach(userGroup);
+            var userGroup = dsl.newRecord(USER_GROUP);
+            userGroup.setUserId(user.getId());
+            userGroup.setGroupId(group.getId());
             userGroup.store();
+
+            if (locale != null) {
+                sendConfirmationEmail(user, locale);
+            }
+
+            return user;
         } else {
             throw new IllegalStateException("USER role does not exist!");
         }
-
-        if (locale != null) {
-            sendConfirmationEmail(user, locale);
-        }
-
-        return user;
     }
 
     public void sendConfirmationEmail(SecurityUserRecord user, Locale locale) {
-        try {
-            Email from = new Email("noreply@jtaf.ch");
-            Email to = new Email(user.getEmail());
-            Content content = new Content("text/plain", i18n.getTranslation("Confirm.Email.Body", locale, publicAddress, user.getConfirmationId()));
-            Mail mail = new Mail(from, i18n.getTranslation("Confirm.Email.Subject", locale), to, content);
-
-            Request request = new Request();
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-
-            Response response = sendGrid.api(request);
-            if (response.getStatusCode() != 202) {
-                throw new RuntimeException(response.getStatusCode() + " : " + response.getBody());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        var message = new SimpleMailMessage();
+        message.setFrom("no-reply@jtaf.ch");
+        message.setTo(user.getEmail());
+        message.setSubject(i18n.getTranslation("Confirm.Email.Subject", locale));
+        message.setText(i18n.getTranslation("Confirm.Email.Body", locale, publicAddress, user.getConfirmationId()));
+        mailSender.send(message);
     }
 
     @Transactional
--- src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
+++ src/test/java/ch/jtaf/service/CompetitionRankingServiceTest.java
@@ -2,11 +2,11 @@ package ch.jtaf.service;
 
 import ch.jtaf.reporting.data.CompetitionRankingData;
 import ch.jtaf.reporting.data.EventsRankingData;
-import com.sendgrid.SendGrid;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CompetitionRankingServiceTest {
 
     @MockBean
-    private SendGrid sendGrid;
+    private JavaMailSender javaMailSender;
 
     @Autowired
     private CompetitionRankingService competitionRankingService;
